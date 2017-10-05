0a6a21f6e0f7f57b920a52f7523fc37ac6f23b80
Guest User <Guest@Epicodus-3A.local>
2017-10-04 17:09:33 -0700

    add working weather api integration

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -24,101 +24,124 @@ function geocodeAddress(geocoder, resultsMap) {
     }
   });
 }
-      function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 45.5231, lng: -122.6765},
-          zoom: 15,
-          mapTypeId: 'roadmap'
-        });
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 45.5231, lng: -122.6765},
+    zoom: 15,
+    mapTypeId: 'roadmap'
+  });
 
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
 
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
 
-          if (places.length == 0) {
-            return;
-          }
+    if (places.length == 0) {
+      return;
+    }
 
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
 
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
       }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
 
 
       //weather api
 
-      $(document).ready(function(){
+$(document).ready(function(){
 
-        var api = "http://api.openweathermap.org/data/2.5/weather?id=5746545&appid=e81b1e7893cab4f428f0629118516293";
+  var api = "http://api.openweathermap.org/data/2.5/weather?id=5746545&appid=e81b1e7893cab4f428f0629118516293";
 
-        $.getJSON(api, function(data){
-        var fTemp;
-        var cTemp;
-        var kTemp;
-        var weatherType;
-        
-        var weatherType = data.weather[0].description;
-        var kTemp = data.main.temp;
-        var windspeed = data.wind.speed;
-        var city= data.name;
+  $.getJSON(api, function(data){
+  var fTemp;
+  var cTemp;
+  var kTemp;
+  var weatherType;
+  var tempSwap = true;
 
-        fTemp= (kTemp)*(9/5)-459.67;
-        cTemp= kTemp-273;
+  var weatherType = data.weather[0].description;
+  var kTemp = data.main.temp;
+  var windspeed = data.wind.speed;
+  var city= data.name;
 
-        // console.log(kTemp);
-        // console.log(cTemp)
-        // console.log(fTemp)
-        // console.log(city)
-        // console.log(weatherType)
-        $("#city").html(city);
-        $("#weatherType").html(weatherType);
-        $("#fTemp").html(fTemp);
-        $("#windspeed").html(windspeed);
-        });
-      });
+  fTemp= (kTemp*(9/5)-459.67).toFixed(1) + (" °F");
+  cTemp= (kTemp-273).toFixed(1) + (" °C");
+
+  $("#city").html(city);
+  $("#weatherType").html(weatherType);
+  $("#fTemp").html(fTemp);
+  $("#fTemp").click(function(){
+    if(tempSwap === false){
+      $("#fTemp").html(fTemp);
+      tempSwap = true;
+    }
+    else{
+      $("#fTemp").html(cTemp);
+      tempSwap = false;
+    }
+  })
+  windspeed= (2.237*(windspeed)).toFixed(2) + (" mph")
+  $("#windspeed").html(windspeed);
+
+  if(parseInt(fTemp) >= 80){
+    $('#weather-card').css('background-image', 'url("/img/sunny.jpg")');
+    }
+
+  else if(parseInt(fTemp) >= 70){
+    $('#weather-card').css('background-image', 'url("/img/rain.jpg")');
+    }
+
+  else if(parseInt(fTemp) >= 50){
+    $('#weather-card').css('background-image', 'url("/img/cloudy.jpg")');
+    }
+
+  else if(parseInt(fTemp) > 40){
+    $('#weather-card').css('background-image', 'url("/img/cold.jpg")');
+    };
+  });
+});
